36fce2c468bcec7e455c092f7cec0c3e8274ea60
Dinith Karunanayake <thewarmth07@gmail.com>
2019-07-06 19:15:34 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from 'search-bar.js';
+import VideoScreen from 'video-screen.js';
+import VideoList from 'video-list.js';
+import YoutubeAPIKey from 'youtubeapikey';
+
+
+
+/*
+//code for search-bar.js
+//this.state for initial state
+//this.setState to change state always
+
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      initialState: "React video tutorial";
+      search: " "
+    }
+  }
+  
+  changeSearch(search){
+    this.setState({search});
+  }
+  
+  
+  
+}
+*/
