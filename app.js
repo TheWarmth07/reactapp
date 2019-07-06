722d55b3a1d65320e9cbf0e858b53c4f3351eff1
Dinith Karunanayake <thewarmth07@gmail.com>
2019-07-06 19:09:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+/*
+//javacript math testing
+<script>
+    var game = 0;
+    var p1;
+    var p2;
+    var score1 = 0;
+    var score2 = 0;
+    function add(){
+    if((score1 || score2) < 5){
+    if(game < 10){
+      game++;
+      var p1 = Math.random();
+      var p2 = Math.random();
+      if(p1 > p2){
+        score1++;
+      }else{
+        score2++;
+      }
+      console.log("Game: " + game);
+      console.log("player 1: " + score1);
+        console.log("player 2: " + score2);
+    }}}
+    
+  </script>
+  
+  */
+*/
